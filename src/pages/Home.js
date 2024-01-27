2c4835b7a39122b3c9f3c5059bf936d42f98065f
svivek19 <svivek030503@gmail.com>
2024-01-27 18:45:04 +0530

    fix: home page ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,25 +47,40 @@ const Home = () => {
   }
 
   return (
-    <div className="container mt-28 mx-auto p-4 bg-gray-100">
+    <div className="container mt-28 mx-auto p-4 bg-gray-50">
       {postList.length === 0 ? (
         <h3 className="text-xl font-bold mb-4">No posts added!</h3>
       ) : (
         postList.map((post) => (
-          <div key={post.id} className="bg-white shadow-md p-4 mb-4">
-            <h2 className="text-lg font-bold mb-2 text-indigo-700">{post.title}</h2>
-            <p className="text-gray-700 mb-4">
-              {post.isExpanded ? post.post : `${post.post.slice(0, 100)}...`}
-            </p>
-            <div className="md:flex items-center justify-between mb-2">
-              <p className="text-gray-500 text-sm mb-2 md:mb-0">
-                Author: <span className="text-purple-700">{post.author?.name}</span>
-              </p>
+          <article key={post.id} className="flex bg-white transition mb-4 hover:shadow-xl">
+            <div className="flex flex-1 flex-col justify-between">
+              <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
+                <a href="#">
+                  <h3 className="font-bold uppercase text-gray-900">
+                    {post.title}
+                  </h3>
+                </a>
+
+                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
+                  {post.isExpanded ? post.post : `${post.post.slice(0, 100)}...`}
+                </p>
+
+
+                <p className="text-gray-500 mt-5 text-sm mb-2 md:mb-0">
+                  Author: <span className="text-purple-700">{post.author?.name}</span>
+                </p>
+              </div>
+
+              <div className="sm:flex sm:items-end sm:justify-end">
+                <Link
+                  to={`/post/${post.id}`}
+                  className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
+                >
+                  Read Blog
+                </Link>
+              </div>
             </div>
-            <Link to={`/post/${post.id}`} className="text-blue-500 hover:underline cursor-pointer text-sm">
-              View Post
-            </Link>
-          </div>
+          </article>
         ))
       )}
     </div>
